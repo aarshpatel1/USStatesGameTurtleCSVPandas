90b5a40edbb60ebaa04c0b1a361778c94d872b02
Aarsh Patel <aarsh18patel@gmail.com>
2024-04-24 18:19:16 +0530

    final commit

--- main.py
+++ main.py
@@ -7,36 +7,43 @@ screen.title("US State Game")
 IMAGE = "blank_states_img.gif"
 screen.addshape(IMAGE)
 turtle.shape(IMAGE)
-turtle.hideturtle()
-
-# TODO: convert guess to title case
-answer_state = turtle.textinput(title="Guess the State", prompt="What is the another state name?: ").capitalize()
 
 # TODO: check if guess is among the 50 states
 states_data = pandas.read_csv("50_states.csv")
-print(states_data)
+# print(states_data)
 states = states_data["state"].to_list()
-print(states)
+
+
+# print(states)
 
 
 def write_state_names(state_name, x_cord, y_cord):
     new_turtle = turtle.Turtle()
+    new_turtle.hideturtle()
+    new_turtle.penup()
     new_turtle.goto(x_cord, y_cord)
-    new_turtle.write(state_name, align="center", font=("arial", 10, "bold"))
+    new_turtle.write(state_name)
 
-
-if answer_state in states:
-    # TODO: write correct guess onto map
-    state = states_data[states_data["state"] == answer_state]
-    print(state.x.to_string())
-    print(state.y)
-    # write_state_names(state, state.x, state.y)
-
-# TODO: use a loop to allow the user to keeping guessing
 
 # TODO: record the correct guesses in a list
+correct_answer = []
 
 # TODO: keep track of the score
+score = 0
 
+# TODO: use a loop to allow the user to keeping guessing
+is_game_on = True
+while is_game_on:
+    # TODO: convert guess to title case
+    answer_state = turtle.textinput(title=f"{score}/{len(states)}", prompt="What is the another state name?: ").title()
+    if (answer_state in states) and (answer_state not in correct_answer):
+        # TODO: write correct guess onto map
+        state = states_data[states_data["state"] == answer_state]
+        write_state_names(answer_state, int(state.x), int(state.y))
+        correct_answer.append(answer_state)
+        score += 1
+
+    if score == 50:
+        is_game_on = False
 
 screen.exitonclick()
