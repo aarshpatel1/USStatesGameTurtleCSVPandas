8968dc3174db69ae6219aae29fa6d35950ee3330
Aarsh Patel <aarsh18patel@gmail.com>
2024-04-24 18:56:24 +0530

    final commit

--- main.py
+++ main.py
@@ -27,23 +27,32 @@ def write_state_names(state_name, x_cord, y_cord):
 
 # TODO: record the correct guesses in a list
 correct_answer = []
+left_to_guess = []
 
 # TODO: keep track of the score
 score = 0
 
 # TODO: use a loop to allow the user to keeping guessing
-is_game_on = True
-while is_game_on:
+while score != 50:
     # TODO: convert guess to title case
     answer_state = turtle.textinput(title=f"{score}/{len(states)}", prompt="What is the another state name?: ").title()
+
+    if answer_state == "Exit":
+        # TODO: create and csv file which contains remaining states to guess by user
+        for state in states:
+            if state not in correct_answer:
+                left_to_guess.append(state)
+        remaining_states = pandas.DataFrame(left_to_guess)
+        remaining_states.to_csv("remaining_states.csv")
+        break
+
     if (answer_state in states) and (answer_state not in correct_answer):
         # TODO: write correct guess onto map
         state = states_data[states_data["state"] == answer_state]
-        write_state_names(answer_state, int(state.x), int(state.y))
+        # write_state_names(answer_state, int(state.x), int(state.y))
+        write_state_names(state.state.item(), int(state.x), int(state.y))
+
         correct_answer.append(answer_state)
         score += 1
-
-    if score == 50:
-        is_game_on = False
 
 screen.exitonclick()
